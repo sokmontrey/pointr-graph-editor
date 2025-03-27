0d0378f7abb328232116ab896fdad0799e5d770a
Sokmontrey <sythatsokmontrey@gmail.com>
2025-03-27 15:09:13 -0400

    feat: basic Node and Edge type

--- src/types/Graph.ts
+++ src/types/Graph.ts
@@ -3,3 +3,19 @@ export interface NodeType {
     type: string;
     color: string;
 }
+
+export interface Node {
+    type: NodeType;
+    x: number;
+    y: number;
+}
+
+export interface Edge {
+    from: Node;
+    to: Node;
+}
+
+export interface Graph {
+    nodes: Node[];
+    edges: Edge[];
+}
