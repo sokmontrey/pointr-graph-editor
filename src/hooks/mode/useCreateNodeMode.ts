c892667ca111daa1ca6191df660cfe73b39c4ade
Sokmontrey <sythatsokmontrey@gmail.com>
2025-03-29 23:29:53 -0400

    refac(+fix): remove dragend from mode

--- src/hooks/mode/useCreateNodeMode.ts
+++ src/hooks/mode/useCreateNodeMode.ts
@@ -2,21 +2,18 @@ import { Mode } from "../../types";
 import { NodeType } from "../../types/Graph";
 
 const useCreateNodeMode = (nodeType: NodeType): Mode => {
-  return {
-    name: `Create ${nodeType.name}`,
-    handleMouseMove: (e: MouseEvent) => {
-      console.log("Mouse moved at", e.clientX, e.clientY);
-    },
-    handleDragging: (e: MouseEvent) => {
-      console.log("Dragging at", e.clientX, e.clientY);
-    },
-    handleDragEnd: (e: MouseEvent) => {
-      console.log("Selection ended at", e.clientX, e.clientY);
-    },
-    handleClick: (e: MouseEvent) => {
-      console.log("Click at", e.clientX, e.clientY);
-    },
-  };
+    return {
+        name: `Create ${nodeType.name}`,
+        handleMouseMove: (e: MouseEvent) => {
+            console.log("Mouse moved at", e.clientX, e.clientY);
+        },
+        handleDragging: (e: MouseEvent) => {
+            console.log("Dragging at", e.clientX, e.clientY);
+        },
+        handleClick: (e: MouseEvent) => {
+            console.log("Click at", e.clientX, e.clientY);
+        },
+    };
 };
 
 export default useCreateNodeMode;
--- src/hooks/mode/useModeManager.ts
+++ src/hooks/mode/useModeManager.ts
@@ -2,10 +2,10 @@ import { useState } from 'react';
 import { Mode } from '../../types';
 
 export function useModeManager(initialMode: Mode) {
-  const [mode, setMode] = useState<Mode>(initialMode);
+    const [mode, setMode] = useState<Mode>(initialMode);
 
-  return {
-    mode,
-    setMode,
-  };
+    return {
+        mode,
+        setMode,
+    };
 }
