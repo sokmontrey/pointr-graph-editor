45110c2f07387dce3c9ad85a74552a5e0cdbfc84
Sokmontrey <sythatsokmontrey@gmail.com>
2025-04-03 17:47:44 -0400

    feat: controls for updating image overlay

--- src/components/Controls.tsx
+++ src/components/Controls.tsx
@@ -1,5 +1,7 @@
 import Toolbar from "./Toolbar";
 import { ImageOverlayManager, ModeManager } from "../types";
+import StepperInput from "./input/StepperInput";
+import VectorInput from "./input/VectorInput";
 
 interface ControlsProps {
     modeManager: ModeManager;
@@ -28,6 +30,31 @@ const Controls = ({
                     imageOverlayManager.setImage(image);
                 };
             }}
+        />
+
+        <VectorInput
+            label="Image Position"
+            value={imageOverlayManager.imageOffset}
+            onChange={imageOverlayManager.setImageOffset}
+            step={10}
+        />
+
+        <StepperInput
+            label="Image Scale"
+            value={imageOverlayManager.imageScale}
+            onChange={imageOverlayManager.setImageScale}
+            min={0.01}
+            max={2}
+            step={0.05}
+        />
+
+        <StepperInput
+            label="Image Opacity"
+            value={imageOverlayManager.imageOpacity}
+            onChange={imageOverlayManager.setImageOpacity}
+            min={0}
+            max={1}
+            step={0.05}
         />
     </div>);
 };
--- src/hooks/imageOverlay/useImageOverlayManager.ts
+++ src/hooks/imageOverlay/useImageOverlayManager.ts
@@ -5,10 +5,13 @@ import { ImageOverlayManager } from "../../types";
 export const useImageOverlayManager = (): ImageOverlayManager => {
     const [imageOffset, setImageOffset] = useState<Point>(new Point(0, 0));
     const [imageScale, setImageScale] = useState<number>(1);
+    const [imageOpacity, setImageOpacity] = useState<number>(1);
     const [image, setImage] = useState<HTMLImageElement | null>(null);
 
     const draw = (ctx: CanvasRenderingContext2D) => {
         if (!ctx || !image) return;
+        ctx.globalAlpha = imageOpacity;
+        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
         ctx.drawImage(
             image,
             imageOffset.x,
@@ -23,5 +26,9 @@ export const useImageOverlayManager = (): ImageOverlayManager => {
         setImage,
         setImageOffset,
         setImageScale,
+        imageOffset,
+        imageScale,
+        imageOpacity,
+        setImageOpacity,
     };
 };
