b030bd14a7c7e830b87449b6f6d46b6aad2878d5
Sokmontrey <sythatsokmontrey@gmail.com>
2025-04-03 20:38:49 -0400

    refac: move image overlay controls to a component

--- src/components/Controls.tsx
+++ src/components/Controls.tsx
@@ -1,7 +1,6 @@
 import Toolbar from "./Toolbar";
 import { ImageOverlayManager, ModeManager } from "../types";
-import DragNumberInput from "./input/DragNumberInput";
-import VectorInput from "./input/VectorInput";
+import ImageOverlayControls from "./ImageOverlayControls";
 
 interface ControlsProps {
     modeManager: ModeManager;
@@ -17,46 +16,8 @@ const Controls = ({
             setMode={modeManager.setMode}
         />
 
-        {/* A proptotype for image overlay for now */}
-        <input
-            type="file"
-            accept="image/*"
-            onChange={(e) => {
-                if (!e.target.files) return;
-                const file = e.target.files[0];
-                const image = new Image();
-                image.src = URL.createObjectURL(file);
-                image.onload = () => {
-                    imageOverlayManager.setImage(image);
-                };
-            }}
-        />
-
-        <VectorInput
-            label="Image Position"
-            value={imageOverlayManager.imageOffset}
-            onChange={imageOverlayManager.setImageOffset}
-            step={1}
-        />
-
-        <DragNumberInput
-            label="Image Scale"
-            value={imageOverlayManager.imageScale}
-            onChange={imageOverlayManager.setImageScale}
-            min={0.01}
-            max={2}
-            step={0.01}
-            dragSensitivity={0.01}
-        />
-
-        <DragNumberInput
-            label="Image Opacity"
-            value={imageOverlayManager.imageOpacity}
-            onChange={imageOverlayManager.setImageOpacity}
-            min={0}
-            max={1}
-            step={0.01}
-            dragSensitivity={0.01}
+        <ImageOverlayControls
+            imageOverlayManager={imageOverlayManager}
         />
     </div>);
 };
