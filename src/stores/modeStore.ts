1f49a756b241c5a630030aba193d0512e72041e4
Sokmontrey <sythatsokmontrey@gmail.com>
2025-04-22 17:08:52 -0400

    feat: modeStore

---
+++ src/stores/modeStore.ts
@@ -0,0 +1,17 @@
+﻿import { create } from 'zustand';
+import { IMode, SelectMode } from '../domain/modes'
+
+interface ModeState {
+    mode: IMode;
+}
+
+interface ModeAction {
+    setMode: (mode: IMode) => void;
+}
+
+export const useModeStore = create<
+    ModeState & ModeAction
+>((set) => ({
+    mode: new SelectMode(),
+    setMode: (mode) => set({ mode }),
+}));
--- src/types/index.ts
+++ src/types/index.ts
@@ -17,14 +17,6 @@ export interface RenderBus {
 
 // ================================= Viewport =================================
 
-export interface ViewportSettings {
-    initialX?: number;
-    initialY?: number;
-    initialScale?: number;
-    minScale?: number;
-    maxScale?: number;
-}
-
 export interface ViewportTransform {
     x: number;
     y: number;
@@ -69,11 +61,4 @@ export interface ImageOverlayManager {
 export interface ModeManager {
     mode: Mode;
     setMode: React.Dispatch<React.SetStateAction<Mode>>;
-}
-
-export interface Mode {
-    name: string;
-    handleDragging(e: MouseEvent): void;
-    handleClick(e: MouseEvent): void;
-    handleMouseMove(e: MouseEvent): void;
 }
