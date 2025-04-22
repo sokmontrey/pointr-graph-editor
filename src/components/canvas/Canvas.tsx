febc20d8837cbbd6ae3f1dead7b5b0216f2d059f
Sokmontrey <sythatsokmontrey@gmail.com>
2025-04-22 19:11:26 -0400

    feat: common canvas component

---
+++ src/components/canvas/Canvas.tsx
@@ -0,0 +1,28 @@
+﻿import React from "react";
+
+interface CanvasProps {
+    ref: React.Ref<HTMLCanvasElement>;
+    width?: number;
+    height?: number;
+    disablePointerEvent?: boolean;
+}
+
+const Canvas = ({
+                    ref,
+                    disablePointerEvent = false,
+                }: CanvasProps) => {
+    return (<canvas
+        ref={ref}
+        style={{
+            border: '1px solid #ccc',
+            position: 'absolute',
+            width: '100%',
+            height: '100%',
+            top: 0,
+            left: 0,
+            pointerEvents: disablePointerEvent ? undefined : 'none'
+        }}
+    />)
+};
+
+export default Canvas;
