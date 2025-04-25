52ab5c02b7251de956d00b34b0492da74e884c1a
Sokmontrey <sythatsokmontrey@gmail.com>
2025-04-25 14:17:34 -0400

    feat: CommandManager.ts

---
+++ src/core/commands/CommandManager.ts
@@ -0,0 +1,37 @@
+﻿import {GraphStore} from "../../stores/graph";
+import {ICommand} from "./ICommand.ts";
+
+class CommandManager {
+    private undoStack: ICommand[] = [];
+    private redoStack: ICommand[] = [];
+    private readonly graphStore: GraphStore;
+
+    constructor(store: GraphStore) {
+        this.graphStore = store;
+    }
+
+    execute(command: ICommand) {
+        command.setGraphStore(this.graphStore);
+        command.execute();
+        this.undoStack.push(command);
+        this.redoStack = [];
+    }
+
+    undo() {
+        if (this.undoStack.length > 0) {
+            const command = this.undoStack.pop()!;
+            command.undo();
+            this.redoStack.push(command);
+        }
+    }
+
+    redo() {
+        if (this.redoStack.length > 0) {
+            const command = this.redoStack.pop()!;
+            command.execute();
+            this.undoStack.push(command);
+        }
+    }
+}
+
+export default CommandManager;
