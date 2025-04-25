540ca3ad9efc72e7607899fca1424b3be6748d45
Sokmontrey <sythatsokmontrey@gmail.com>
2025-04-24 21:59:55 -0400

    feat: created ModeOverlayCanvas.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,24 +5,26 @@ import ImageOverlayControl from "./components/controls/ImageOverlayControl.tsx";
 import ImageOverlayCanvas from "./components/canvas/ImageOverlayCanvas.tsx";
 import BulletGrid from "./components/canvas/BulletGrid.tsx";
 import GridControl from "./components/controls/gridControl.tsx";
+import ModeOverlayCanvas from "./components/canvas/ModeOverlayCanvas.tsx";
 
 export default function App() {
-    return ( <>
+    return (<>
         <div style={{
             position: 'fixed',
             top: 0,
             left: 0,
             zIndex: 1000,
         }}>
-            <ModeControl />
-            <ImageOverlayControl />
-            <GridControl />
+            <ModeControl/>
+            <ImageOverlayControl/>
+            <GridControl/>
         </div>
 
         <div style={{position: 'relative'}}>
-            <MainCanvas zIndex={100} />
-            <BulletGrid zIndex={10} />
-            <ImageOverlayCanvas zIndex={0} />
+            <ModeOverlayCanvas zIndex={1000}/>
+            <MainCanvas zIndex={100}/>
+            <BulletGrid zIndex={10}/>
+            <ImageOverlayCanvas zIndex={0}/>
         </div>
     </>);
 }
---
+++ src/components/canvas/ModeOverlayCanvas.tsx
@@ -0,0 +1,23 @@
+﻿import Canvas, {ZIndexProps} from "./Canvas.tsx";
+import {useRef} from "react";
+import {useRenderingBus} from "../../hooks/useRenderingBus.ts";
+import {useRenderingHandler} from "../../hooks/useRenderingHandler.ts";
+import {useViewportStore} from "../../stores/viewportStore.ts";
+
+const ModeOverlayCanvas = ({ zIndex }: ZIndexProps) => {
+    const ref = useRef<HTMLCanvasElement | null>(null);
+
+    const viewport = useViewportStore();
+
+    const renderingBus = useRenderingBus();
+
+    useRenderingHandler(ref, renderingBus, viewport);
+
+    return ( <Canvas
+        zIndex={zIndex}
+        ref={ref}
+        disablePointerEvent={false}
+    /> );
+}
+
+export default ModeOverlayCanvas;
