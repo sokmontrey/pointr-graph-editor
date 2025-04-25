38662cb15eb9529cd7e8e25332477e1203d0942a
Sokmontrey <sythatsokmontrey@gmail.com>
2025-04-24 21:07:54 -0400

    refac: dynamic dotSize

--- src/components/canvas/BulletGrid.tsx
+++ src/components/canvas/BulletGrid.tsx
@@ -3,15 +3,16 @@ import {ZIndexProps} from "./Canvas.tsx";
 
 const BulletGrid = ({ zIndex }: ZIndexProps) => {
     const viewport = useViewportStore();
-    const baseGridSize = 10; // TODO: grid store & control
-    const gridSize = Math.max(1, baseGridSize * viewport.scale);
+    const baseGapSize = 10; // TODO: grid store & control
+    const gapSize = Math.max(1, baseGapSize * viewport.scale);
+    const dotSize = 0.8 + Math.max(0.2, viewport.scale * 0.2);
 
     return <div
         className="absolute -z-10 inset-0 h-full w-full"
         style={{
-            backgroundImage: 'radial-gradient(circle, #73737388 1px, transparent 1px)',
-            backgroundSize: `${gridSize}px ${gridSize}px`,
-            backgroundPosition: `${viewport.offset.x % gridSize}px ${viewport.offset.y % gridSize}px`,
+            backgroundImage: `radial-gradient(circle, #73737399 ${dotSize}px, transparent ${dotSize}px)`,
+            backgroundSize: `${gapSize}px ${gapSize}px`,
+            backgroundPosition: `${viewport.offset.x % gapSize}px ${viewport.offset.y % gapSize}px`,
             position: 'absolute',
             top: 0,
             left: 0,
