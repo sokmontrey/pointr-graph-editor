d4da91973f844f0663b9d497e6616931cc506011
Sokmontrey <sythatsokmontrey@gmail.com>
2025-04-24 20:46:29 -0400

    feat: bullets grid overlay

--- src/components/canvas/OverlayCanvas.tsx
+++ src/components/canvas/OverlayCanvas.tsx
@@ -18,7 +18,26 @@ const OverlayCanvas = () => {
 
     useRenderingHandler(ref, renderingBus, viewport);
 
-    return ( <Canvas ref={ref} /> );
+    const baseGridSize = 10; // TODO: grid store & control
+    const gridSize = Math.max(1, baseGridSize * viewport.scale);
+
+    return (<>
+        <div
+            className="absolute -z-10 inset-0 h-full w-full"
+            style={{
+                backgroundImage: 'radial-gradient(circle, #73737388 1px, transparent 1px)',
+                backgroundSize: `${gridSize}px ${gridSize}px`,
+                backgroundPosition: `${viewport.offset.x % gridSize}px ${viewport.offset.y % gridSize}px`,
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                width: '100%',
+                height: '100vh',
+            }}
+        />
+
+        <Canvas ref={ref}/>
+    </>);
 }
 
 export default OverlayCanvas;
