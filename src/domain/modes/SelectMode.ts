a5e13e9bc4ef873d299862abf0423a48a1d5b299
Sokmontrey <sythatsokmontrey@gmail.com>
2025-04-26 15:37:13 -0400

    refac: better node vs edge hovering priority

--- src/domain/modes/SelectMode.ts
+++ src/domain/modes/SelectMode.ts
@@ -10,6 +10,8 @@ export class SelectMode implements IMode {
     private hoveredNode: Node | null = null;
     private hoveredEdge: [Edge, Segment] | null = null;
 
+    // TODO: store this in a store for outside access
+    // (Keyboard shortcuts, etc)
     private selectedNode: Node | null = null;
     private selectedEdge: [Edge, Segment] | null = null;
 
@@ -20,21 +22,28 @@ export class SelectMode implements IMode {
 
     handleMouseMove(props: EventPropMap["mousemove"]): void {
         this.hoveredNode = this.nodeStore.getHoveredNode(props.mousePos);
-        this.hoveredEdge = this.edgeStore.getHoveredEdge(props.mousePos);
+        if (this.hoveredNode) {
+            this.hoveredEdge = null;
+        } else {
+            this.hoveredEdge = this.edgeStore.getHoveredEdge(props.mousePos);
+        }
     }
 
-    handleDragging(props: EventPropMap["dragging"]): void {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    handleDragging(_props: EventPropMap["dragging"]): void {
     }
 
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     handleClick(_props: EventPropMap["click"]): void {
         if (this.hoveredNode) {
             this.selectedNode = this.hoveredNode;
+            this.selectedEdge = null;
             return;
         }
 
         if (this.hoveredEdge) {
             this.selectedEdge = this.hoveredEdge;
+            this.selectedNode = null;
             return;
         }
 
@@ -43,47 +52,29 @@ export class SelectMode implements IMode {
     }
 
     draw(ctx: CanvasRenderingContext2D): void {
-        this.drawHoveredNode(ctx);
-        this.drawHoveredEdge(ctx);
-        this.drawSelectedNode(ctx);
-        this.drawSelectedEdge(ctx);
+        ctx.lineWidth = 2;
+        this.drawNode(ctx, this.hoveredNode, 'red');
+        this.drawEdge(ctx, this.hoveredEdge, 'red');
+        this.drawNode(ctx, this.selectedNode, 'cyan');
+        this.drawEdge(ctx, this.selectedEdge, 'cyan');
     }
 
-    private drawHoveredNode(ctx: CanvasRenderingContext2D): void {
-        if (!this.hoveredNode) return;
+    private drawNode(ctx: CanvasRenderingContext2D, node: Node | null, color: string): void {
+        if (!node) return;
         ctx.beginPath();
-        ctx.arc(this.hoveredNode.position.x, this.hoveredNode.position.y, 10, 0, 2 * Math.PI);
-        ctx.strokeStyle = 'red';
+        ctx.arc(node.position.x, node.position.y, 10, 0, 2 * Math.PI);
+        ctx.strokeStyle = color;
         ctx.stroke();
     }
 
-    private drawHoveredEdge(ctx: CanvasRenderingContext2D): void {
-        if (!this.hoveredEdge) return;
+    private drawEdge(ctx: CanvasRenderingContext2D, edge: [Edge, Segment] | null, color: string): void {
+        if (!edge) return;
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        const [_edge, segment] = this.hoveredEdge;
+        const [_edge, segment] = edge;
         ctx.beginPath();
         ctx.moveTo(segment.from.x, segment.from.y);
         ctx.lineTo(segment.to.x, segment.to.y);
-        ctx.strokeStyle = 'red';
-        ctx.stroke();
-    }
-
-    private drawSelectedNode(ctx: CanvasRenderingContext2D): void {
-        if (!this.selectedNode) return;
-        ctx.beginPath();
-        ctx.arc(this.selectedNode.position.x, this.selectedNode.position.y, 10, 0, 2 * Math.PI);
-        ctx.strokeStyle = 'green';
-        ctx.stroke();
-    }
-
-    private drawSelectedEdge(ctx: CanvasRenderingContext2D): void {
-        if (!this.selectedEdge) return;
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        const [_edge, segment] = this.selectedEdge;
-        ctx.beginPath();
-        ctx.moveTo(segment.from.x, segment.from.y);
-        ctx.lineTo(segment.to.x, segment.to.y);
-        ctx.strokeStyle = 'green';
+        ctx.strokeStyle = color;
         ctx.stroke();
     }
 }
