33e8b89619a0b801a27c37a42c5c0b6c42531ca6
Sokmontrey <sythatsokmontrey@gmail.com>
2025-04-26 17:00:14 -0400

    feat: selectionState.ts

---
+++ src/stores/main/selectionState.ts
@@ -0,0 +1,22 @@
+﻿import {create} from 'zustand';
+
+export interface SelectionState {
+    selectedNode: string | null;
+    selectedEdge: string | null;
+}
+
+export interface SelectionAction {
+    setSelectedNode: (nodeId: string | null) => void;
+    setSelectedEdge: (edgeId: string | null) => void;
+    clear: () => void;
+}
+
+export type SelectionStore = SelectionState & SelectionAction;
+
+export const useSelectionStore = create<SelectionStore>((set) => ({
+    selectedNode: null,
+    selectedEdge: null,
+    setSelectedNode: (nodeId) => set({selectedNode: nodeId}),
+    setSelectedEdge: (edgeId) => set({selectedEdge: edgeId}),
+    clear: () => set({selectedNode: null, selectedEdge: null}),
+}));
