fa1ac3c0c949d7aa74eeef145abb5f5605d515e1
Sokmontrey <sythatsokmontrey@gmail.com>
2025-04-26 22:27:56 -0400

    feat: attach selection to event bus

---
+++ src/hooks/attachments/useSelectionEventAttachment.ts
@@ -0,0 +1,23 @@
+﻿import {EventBus} from "../event";
+import {SelectionStore} from "../../stores/main/selectionStore.ts";
+import {useEffect} from "react";
+
+export const useSelectionEventAttachment = (eventBus: EventBus, selectionStore: SelectionStore) => {
+    useEffect(() => {
+        const handleDelete = () => {
+            selectionStore.delete();
+        };
+
+        const unsubscribes = [
+            eventBus.subscribe('keypress', ({ key }) => {
+                if (key === 'Delete') {
+                    handleDelete();
+                }
+            }),
+        ];
+
+        return () => {
+            unsubscribes.forEach((unsubscribe) => unsubscribe());
+        };
+    }, [eventBus, selectionStore]);
+};
